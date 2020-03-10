fbbf53e93c51541fadbb960e7732147d497d774b
willemcossey <willem.cossey@student.kuleuven.be>
2020-03-10 15:20:54 +0100

    start of forum queries

--- faq_forum/forum.py
+++ faq_forum/forum.py
@@ -1,4 +1,13 @@
+from .create_faqforum import *
 
+"""
+    Give the answer to the most similar question in the database.
+""""
 def get_answer(question):
+    get_connection("C:/sqlite/db")
 
-def save_question(question):
+
+
+def get_all_answered_questions(database_name):
+
+
