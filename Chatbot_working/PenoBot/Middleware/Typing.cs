5ad1874a891404f1cdfdb4937b301b8be8fc46fd
Joren Van Hecke <joren.vanhecke@student.kuleuven.be>
2020-04-22 16:33:51 +0200

    Await typing dots

--- Chatbot_working/PenoBot/Middleware/Typing.cs
+++ Chatbot_working/PenoBot/Middleware/Typing.cs
@@ -17,7 +17,7 @@ namespace PenoBot.Middleware
             var typingMsg = context.Activity.CreateReply();
             typingMsg.Type = ActivityTypes.Typing;
             typingMsg.Text = null;
-            context.SendActivityAsync(typingMsg);
+            await context.SendActivityAsync(typingMsg);
 
             await next(cancellationToken);
         }
